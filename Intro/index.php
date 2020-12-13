00523c7509263c973c7d97b187175d8f35f4ed9b
Susanna <siusi.villa@gmail.com>
2020-12-13 17:28:35 +0100

    Sintassi base

---
+++ Intro/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+    <html>
+ <head>
+     <title>Hello World</title>
+     <meta charset="utf-8">
+ </head>
+<body>
+
+<!-- commento <?php print_r($_SERVER)?> -->
+
+<?php // Apertura php
+  // Costrutto echo , stampa singola
+  echo '<h1>Hello world!</h1>';
+    
+  
+  // Commento su singola linea
+  
+  # ECHO "DUE";
+
+  /*
+    Questo è un altro modo di scrivere commenti
+    ma su più righe
+  */
+?>
+
+<h2><?='CORSO PHP'?></h2> <!-- sintassi echo con apertura tag php uniti -->
+
+</body>
+</html>
