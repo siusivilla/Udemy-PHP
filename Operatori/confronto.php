27983be6e6c74868676325052129b91d31eb6b97
Susanna <siusi.villa@gmail.com>
2021-01-17 19:56:36 +0100

    Operatori di confronto

--- Operatori/confronto.php
+++ Operatori/confronto.php
@@ -1,6 +1,21 @@
 <?php include_once('../menu.php'); include_once('sottomenu.php'); ?>
 
 <?php
+/* 
+  $a == $b	Uguale	TRUE se $a è uguale a $b dopo la manipolazione dei tipi.
+  $a === $b	Identico	TRUE se $a è uguale a $b, ed sono dello stesso tipo.
+  $a != $b	Diversi	TRUE se $a è diverso da $b dopo la manipolazione dei tipi.
+  $a <> $b	Diversi	TRUE se $a è diverso da $b dopo la manipolazione dei tipi.
+  $a !== $b	Non identici	TRUE se $a è diverso da $b, o se non sono dello stesso tipo.
+  $a < $b	Minore	TRUE se $a è strettamente minore di $b.
+  $a > $b	Maggiore	TRUE se $a è strettamente maggiore di $b.
+  $a <= $b	Minore o uguale	TRUE se $a è minore o uguale a $b.
+  $a >= $b	Maggiore o uguale	TRUE se $a è maggiore o uguale a $b.
+  $a <=> $b Spaceship . Disponibile  PHP 7.  -1 se il primo è minore, 1 se è maggiore o 0 se sono uguale
+  $a ?? $b ?? $c Null coalescing .Disponibile PHP 7.  Ritorna il primo valore not null
+  (condizione)? : valore2 
+*/
+
 $a = '1';
 $b = 1;
 $d = null;
